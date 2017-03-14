b48fa975f98683efb9cf3fb2766d7d80c51b5a2e
Mateusz Knitter <knicio123@gmail.com>
2017-03-14 22:16:38 +0100

    Gotowe testy

--- src/introductionHamcrest/KolkoKrzyzyk.java
+++ src/introductionHamcrest/KolkoKrzyzyk.java
@@ -19,7 +19,7 @@ public class KolkoKrzyzyk {
          * 0 dla remisu
          * 3 błąd
          */
-        public static int checkResult(char table[][]) {
+        public static int checkResult(char table[][], String ruch) {
                 int zmienna=3;
                 if ((table[0][0] == table[1][0] && table[1][0] == table[2][0] && table[2][0] != ' ') ||
                         (table[0][1] == table[1][1] && table[1][1] == table[2][1] && table[2][1] != ' ') ||
@@ -29,9 +29,9 @@ public class KolkoKrzyzyk {
                         (table[2][0] == table[2][1] && table[2][1] == table[2][2] && table[2][2] != ' ') ||
                         (table[0][0] == table[1][1] && table[1][1] == table[2][2] && table[2][2] != ' ') ||
                         (table[0][2] == table[1][1] && table[1][1] == table[2][0] && table[2][0] != ' ')) {
-                        if (user_one) {
+                        if (ruch == "X") {
                                 zmienna=1;
-                        } else {
+                        }else{
                                 zmienna=2;
                         }
                         return zmienna;
@@ -118,31 +118,4 @@ public class KolkoKrzyzyk {
                         }
                 }
         }
-
-/*
-        private static void printTable() {
-                System.out.println();
-                System.out.println();
-                System.out.println("    1 2 3");
-                System.out.println("a   " + table[0][0]+ "|" + table[0][1]+ "|" + table[0][2]);
-                System.out.println("b   " + table[1][0]+ "|" + table[1][1]+ "|" + table[1][2]);
-                System.out.println("c   " + table[2][0]+ "|" + table[2][1]+ "|" + table[2][2]);
-
-        }
-        */
-/*
-        public static void main(String[] args) {
-
-                while (!checkResult()) {
-                        printTable();
-                        if (user_one) {
-                                user_oneMove();
-                        } else {
-                                user_twoMove();
-                        }
-                }
-                printTable();
-        }
-*/
-
 }
--- test/introductionHamcrest/KolkoKrzyzykTest.java
+++ test/introductionHamcrest/KolkoKrzyzykTest.java
@@ -29,66 +29,119 @@ public class KolkoKrzyzykTest {
 	@Test
 	public void ifItsDraw() throws Exception {
 		KolkoKrzyzyk kolkoKrzyzyk = new KolkoKrzyzyk();
-		char table[][] = {{'X', 'O', 'X'},
+		char table1[][] = {{'X', 'O', 'X'},
 				{'X', 'O', 'O'},
 				{'O', 'X', 'O'}};
-		int cos=kolkoKrzyzyk.checkResult(table);
+		int cos=kolkoKrzyzyk.checkResult(table1,"O");
 		assertThat(0,equalTo(cos));
 	}
 	@Test
 	public void ifItsWinPlayerX() throws Exception {
 		KolkoKrzyzyk kolkoKrzyzyk = new KolkoKrzyzyk();
-		char table[][] = {{'X', 'O', 'X'},
+		char table2[][] = {{'X', 'O', 'X'},
 				{'X', 'O', 'O'},
 				{'X', ' ', ' '}};
-		int cos=kolkoKrzyzyk.checkResult(table);
+		int cos=kolkoKrzyzyk.checkResult(table2,"X");
 		assertThat(1,is(cos));
 	}
 	@Test
 	public void ifItsWinPlayerO() throws Exception {
 		KolkoKrzyzyk kolkoKrzyzyk = new KolkoKrzyzyk();
-		char table[][] = {{'X', 'O', 'O'},
+		char table3[][] = {{'X', 'O', 'O'},
 				{'X', 'O', 'X'},
 				{'O', ' ', ' '}};
-		int cos=kolkoKrzyzyk.checkResult(table);
-		assertThat(1,is(cos));
+		int cos=kolkoKrzyzyk.checkResult(table3,"O");
+		assertThat(2,is(cos));
 	}
 	@Test
 	public void ifItsBadtMovePlayerOne() throws Exception {
 		KolkoKrzyzyk kolkoKrzyzyk = new KolkoKrzyzyk();
-		char table[][] = {{'X', 'O', 'O'},
+		char table4[][] = {{'X', 'O', 'O'},
 				{'X', 'O', 'X'},
 				{' ', ' ', ' '}};
-		boolean badmove = kolkoKrzyzyk.user_oneMove(table,"a1");
+		boolean badmove = kolkoKrzyzyk.user_oneMove(table4,"a1");
 		assertThat(true, is(not(equalTo(badmove))));
 	}
 	@Test
 	public void ifItsCorrectMovePlayerOne() throws Exception {
 		KolkoKrzyzyk kolkoKrzyzyk = new KolkoKrzyzyk();
-		char table[][] = {{'X', 'O', 'O'},
+		char table5[][] = {{'X', 'O', 'O'},
 				{'X', ' ', ' '},
 				{' ', ' ', 'X'}};
-		boolean goodmove = kolkoKrzyzyk.user_oneMove(table,"b2");
+		boolean goodmove = kolkoKrzyzyk.user_oneMove(table5,"b2");
 		assertThat(true,is(equalTo(goodmove)));
 	}
 
 	@Test
 	public void ifItsBadtMovePlayerTwo() throws Exception {
 		KolkoKrzyzyk kolkoKrzyzyk = new KolkoKrzyzyk();
-		char table[][] = {{'X', 'O', 'O'},
+		char table6[][] = {{'X', 'O', 'O'},
 				{'X', 'O', 'X'},
 				{' ', ' ', ' '}};
-		boolean badmove = kolkoKrzyzyk.user_twoMove(table,"a1");
+		boolean badmove = kolkoKrzyzyk.user_twoMove(table6, "a1");
 		assertThat(true, is(not(equalTo(badmove))));
 	}
 	@Test
 	public void ifItsCorrectMovePlayerTwo() throws Exception {
 		KolkoKrzyzyk kolkoKrzyzyk = new KolkoKrzyzyk();
-		char table[][] = {{'X', 'O', 'O'},
+		char table7[][] = {{'X', 'O', 'O'},
 				{'X', ' ', ' '},
 				{' ', ' ', 'X'}};
-		boolean goodmove = kolkoKrzyzyk.user_twoMove(table,"b2");
+		boolean goodmove = kolkoKrzyzyk.user_twoMove(table7, "b2");
 		assertThat(true,is(equalTo(goodmove)));
+	}
+	@Test
+	public void ifItsCrossWin() throws Exception {
+		KolkoKrzyzyk kolkoKrzyzyk = new KolkoKrzyzyk();
+		char table8[][] = {{'X', 'O', 'O'},
+				{'O', 'X', ' '},
+				{'O', ' ', 'X'}};
+		int crossmove = kolkoKrzyzyk.checkResult(table8,"X");
+		assertThat(1,equalTo(crossmove));
+	}@Test
+	 public void ifItsCrossRevertWin() throws Exception {
+		KolkoKrzyzyk kolkoKrzyzyk = new KolkoKrzyzyk();
+		char table9[][] = {{'O', 'O', 'X'},
+				{'O', 'X', 'O'},
+				{'X', ' ', 'X'}};
+		int crossmove = kolkoKrzyzyk.checkResult(table9,"X");
+		assertThat(1,equalTo(crossmove));
+	}
+	@Test
+	public void ifItsStraightWin() throws Exception {
+		KolkoKrzyzyk kolkoKrzyzyk = new KolkoKrzyzyk();
+		char table11[][] = {{' ', 'X', 'O'},
+				{' ', 'X', ' '},
+				{'O', 'X', 'O'}};
+		int straightmove = kolkoKrzyzyk.checkResult(table11,"X");
+		assertThat(1, equalTo(straightmove));
+	}
+
+	@Test
+	public void ifItsCrossWin2() throws Exception {
+		KolkoKrzyzyk kolkoKrzyzyk = new KolkoKrzyzyk();
+		char table22[][] = {{'O', 'X', ' '},
+				{'X', 'O', 'X'},
+				{' ', ' ', 'O'}};
+		int crossmove = kolkoKrzyzyk.checkResult(table22,"O");
+		assertThat(2,equalTo(crossmove));
+	}@Test
+	 public void ifItsCrossRevertWin2() throws Exception {
+		KolkoKrzyzyk kolkoKrzyzyk = new KolkoKrzyzyk();
+		char table33[][] = {{' ', 'X', 'O'},
+				{' ', 'O', 'X'},
+				{'O', ' ', 'X'}};
+		int crossmove = kolkoKrzyzyk.checkResult(table33,"O");
+		assertThat(2,equalTo(crossmove));
+	}
+	@Test
+	public void ifItsStraightWin2() throws Exception {
+		KolkoKrzyzyk kolkoKrzyzyk = new KolkoKrzyzyk();
+		char table44[][] = {{'X', 'O', 'O'},
+				{'X', 'O', 'X'},
+				{' ', 'O', ' '}};
+		int straightmove = kolkoKrzyzyk.checkResult(table44,"O");
+		assertThat(2, equalTo(straightmove));
 	}
 
 	@After
